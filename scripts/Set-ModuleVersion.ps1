9f2bacb5acdaad2de6bf180fa1194bf075839fe6
Marius Storhaug <marstor@hotmail.com>
2023-08-19 23:23:05 +0200

    Fix script

--- scripts/Set-ModuleVersion.ps1
+++ scripts/Set-ModuleVersion.ps1
@@ -1,23 +1,28 @@
 [CmdletBinding()]
 param (
     [Parameter()]
-    [string] $ModuleName = "GitHub"
+    [string] $ModuleName = 'GitHub'
 )
 
-[Version]$NewVersion = "0.0.0"
+[Version]$NewVersion = '0.0.0'
 
-$OnlineVersion = [Version](Find-Module $ModuleName).Version
+try {
+    $OnlineVersion = [Version](Find-Module $ModuleName).Version
+} catch {
+    Write-Warning 'Could not find module online. Using 0.0.0'
+    $OnlineVersion = [Version]'0.0.0'
+}
 Write-Warning "Online: $($OnlineVersion.ToString())"
 $ManifestVersion = [Version](Test-ModuleManifest ".\src\$ModuleName\$ModuleName.psd1").Version
 Write-Warning "Manifest: $($ManifestVersion.ToString())"
 
-if($ManifestVersion.Major -gt $OnlineVersion.Major) {
+if ($ManifestVersion.Major -gt $OnlineVersion.Major) {
     $NewVersionMajor = $ManifestVersion.Major
     $NewVersionMinor = 0
     $NewVersionBuild = 0
 } else {
     $NewVersionMajor = $OnlineVersion.Major
-    if($ManifestVersion.Minor -gt $OnlineVersion.Minor) {
+    if ($ManifestVersion.Minor -gt $OnlineVersion.Minor) {
         $NewVersionMinor = $ManifestVersion.Minor
         $NewVersionBuild = 0
     } else {
@@ -25,7 +30,7 @@ if($ManifestVersion.Major -gt $OnlineVersion.Major) {
         $NewVersionBuild = $OnlineVersion.Build + 1
     }
 }
-[Version]$NewVersion = [version]::new($NewVersionMajor,$NewVersionMinor,$NewVersionBuild)
+[Version]$NewVersion = [version]::new($NewVersionMajor, $NewVersionMinor, $NewVersionBuild)
 Write-Warning "NewVersion: $($NewVersion.ToString())"
 
 Update-ModuleManifest -Path ".\src\$ModuleName\$ModuleName.psd1" -ModuleVersion $NewVersion -ErrorAction Continue
