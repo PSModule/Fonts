69554017985b41c1349bfdc49ee97023de4ecb91
Marius Storhaug <marstor@hotmail.com>
2023-08-19 22:56:33 +0200

    Refresh

---
+++ scripts/Publish-Module.ps1
@@ -0,0 +1,15 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter()]
+    [string] $ModuleName,
+
+    [Parameter()]
+    [string] $APIKey
+)
+
+$SRCPath = Get-Item -Path .\src\ | Select-Object -ExpandProperty FullName
+$env:PSModulePath += ":$SRCPath"
+$env:PSModulePath -Split ':'
+
+.\scripts\Set-ModuleVersion.ps1 -ModuleName $ModuleName -Verbose
+Publish-Module -Path "src/$ModuleName" -NuGetApiKey $APIKey -Verbose
