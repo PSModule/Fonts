ac2b863e5ee0a2b4d0d63df480174e498f459932
Marius Storhaug <marstor@hotmail.com>
2024-03-12 20:19:48 +0100

    Add tests

---
+++ tests/Fonts.Tests.ps1
@@ -0,0 +1,11 @@
+﻿Describe 'Fonts' {
+    Context 'Module' {
+        It 'The module should be available' {
+            Get-Module -Name 'Fonts' -ListAvailable | Should -Not -BeNullOrEmpty
+            Write-Verbose (Get-Module -Name 'Fonts' -ListAvailable | Out-String) -Verbose
+        }
+        It 'The module should be imported' {
+            { Import-Module -Name 'Fonts' } | Should -Not -Throw
+        }
+    }
+}
