1fcb4f546ebad3e27fad67846365de9585f5eaad
Marius Storhaug <marstor@hotmail.com>
2024-03-12 22:25:28 +0100

    Squash

---
+++ examples/General.ps1
@@ -0,0 +1,19 @@
+﻿<#
+  .SYNOPSIS
+    This is a general example of how to use the module.
+#>
+
+# Import the module
+Import-Module -Name 'PSModule'
+
+# Define the path to the font file
+$FontFilePath = 'C:\Fonts\CodeNewRoman\CodeNewRomanNerdFontPropo-Regular.tff'
+
+# Install the font
+Install-Font -Path $FontFilePath -Verbose
+
+# List installed fonts
+Get-Font -Name 'CodeNewRomanNerdFontPropo-Regular'
+
+# Uninstall the font
+Get-Font -Name 'CodeNewRomanNerdFontPropo-Regular' | Uninstall-Font -Verbose
