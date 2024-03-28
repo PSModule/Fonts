0e43c018fb06ec99d02df52ad06b73e13cc68cca
Marius Storhaug <marstor@hotmail.com>
2024-03-28 22:14:34 +0100

    align

--- tests/Fonts.Tests.ps1
+++ tests/Fonts.Tests.ps1
@@ -1,11 +1,18 @@
-﻿Describe 'Fonts' {
+﻿[CmdletBinding()]
+Param(
+    # Path to the module to test.
+    [Parameter()]
+    [string] $Path
+)
+
+Describe 'Fonts' {
     Context 'Module' {
         It 'The module should be available' {
             Get-Module -Name 'Fonts' -ListAvailable | Should -Not -BeNullOrEmpty
             Write-Verbose (Get-Module -Name 'Fonts' -ListAvailable | Out-String) -Verbose
         }
         It 'The module should be imported' {
-            { Import-Module -Name 'Fonts' } | Should -Not -Throw
+            { Import-Module -Name 'Fonts' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
         }
     }
 }
