d50946e08263ec7f2333c2d1a556ce5b97014442
Marius Storhaug <marstor@hotmail.com>
2024-04-06 11:51:34 +0200

    Add data for supported fonts

--- src/private/common.ps1
+++ src/private/common.ps1
@@ -27,3 +27,41 @@ $script:OS = if ([System.Environment]::OSVersion.Platform -eq 'Win32NT') {
 } else {
     throw 'Unsupported OS'
 }
+
+$script:SupportedFonts = @(
+    [pscustomobject]@{
+        Extension   = '.ttf'
+        Type        = 'TrueType'
+        Description = 'TrueType Font'
+    }
+    [pscustomobject]@{
+        Extension   = '.otf'
+        Type        = 'OpenType'
+        Description = 'OpenType Font'
+    }
+    [pscustomobject]@{
+        Extension   = '.ttc'
+        Type        = 'TrueType'
+        Description = 'TrueType Font Collection'
+    }
+    [pscustomobject]@{
+        Extension   = '.pfb'
+        Type        = 'PostScript Type 1'
+        Description = 'PostScript Type 1 Font'
+    }
+    [pscustomobject]@{
+        Extension   = '.pfm'
+        Type        = 'PostScript Type 1'
+        Description = 'PostScript Type 1 Outline Font'
+    }
+    [pscustomobject]@{
+        Extension   = '.woff'
+        Type        = 'Web Open Font Format'
+        Description = 'Web Open Font Format'
+    }
+    [pscustomobject]@{
+        Extension   = '.woff2'
+        Type        = 'Web Open Font Format 2'
+        Description = 'Web Open Font Format 2'
+    }
+)
