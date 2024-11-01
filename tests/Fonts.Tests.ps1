5f3dbdc7440d733c28cf17262a35d420a433fe0d
Marius Storhaug <marstor@hotmail.com>
2024-11-02 00:04:12 +0100

    🩹 [Patch]: Remove redundant parameter from tests (#33)
    ## Description
    
    - Remove redundant parameter from tests
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/Fonts.Tests.ps1
+++ tests/Fonts.Tests.ps1
@@ -1,13 +1,4 @@
-﻿[CmdletBinding()]
-Param(
-    # Path to the module to test.
-    [Parameter()]
-    [string] $Path
-)
-
-Write-Verbose "Path to the module: [$Path]" -Verbose
-
-Describe 'Fonts' {
+﻿Describe 'Fonts' {
     Context 'Function: Get-Font' {
         It 'The function should be available' {
             Get-Command -Name 'Get-Font' | Should -Not -BeNull
