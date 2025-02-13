7e5b381ced6ae17a7b2ef84ddf6125f1d090d6cf
Marius Storhaug <marstor@hotmail.com>
2025-02-13 13:42:06 +0100

    🩹 [Patch]: Move argument completer for Uninstall-Font and Get-Font to a separate file

---
+++ src/functions/completers.ps1
@@ -0,0 +1,9 @@
+﻿Register-ArgumentCompleter -CommandName Uninstall-Font, Get-Font -ParameterName Name -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters
+    if ([string]::IsNullOrEmpty($fakeBoundParameters['Scope'])) {
+        Get-Font -Scope 'CurrentUser' | Where-Object { $_.Name -like "$wordToComplete*" } | Select-Object -ExpandProperty Name
+    } else {
+        Get-Font -Scope $fakeBoundParameters['Scope'] | Where-Object { $_.Name -like "$wordToComplete*" } | Select-Object -ExpandProperty Name
+    }
+}
